abaadc5ae51501ab0d5485ec93e780f92ab6b2ff
Алексей Гарбузов <garbuzov.gar@icloud.com>
2026-01-28 17:00:03 +0300

    feat: product list with discount logic, tests and deploy

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/product-list-react/",
+  plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
     globals: true,
   },
-  plugins: [react()],
 });
